d9156c629ec17ba46d02a011e5b4dc8d8047e822
ypablo <yfedora@gmail.com>
2019-06-07 15:55:16 +0100

    25.3 CRUD

--- server.js
+++ server.js
@@ -1,68 +1,26 @@
 var express = require('express');
-
 var app = express();
 var fs = require('fs')
 var stringifyFile;
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.get('/getNote', function (req, res) {
-    console.log('Otrzymałem żądanie GET do strony głównej');
-    //res.send('JSON:');
-    res.json(test.json)
-});
-
-
-fs.readFile('./test.json', 'utf8', function(err, data) {
-    if (err) throw err;
-    stringifyFile = data
-    res.send(data);
+    fs.readFile('./test.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        stringifyFile = data;
+        res.send(data);
+        console.log("app.get works!")
+    });
 });
 
 app.post('/updateNote/:note', function (req, res) {
-    console.log('Otrzymałem żądanie POST do strony głównej');
-    stringifyFile = req.params.note;
-    res.send('Hello POST!');
-});
-
-
-fs.writeFile('./test.json', stringifyFile, function(err) {
-    If (err) throw err;
-    console.log('file updated');
-});
-
-
-var server = app.listen(3000);
-
-
-/*
-app.get('/', function (req, res) {
-    console.log('Otrzymałem żądanie GET do strony głównej');
-    res.send('Identyfikator, który został dopisany to ' + req.params.id);
+    stringifyFile = stringifyFile + " " + req.params.note;
+    fs.writeFile('./test.json', stringifyFile, function (err) {
+        // If(err) throw err;
+        console.log('file updated');
+    });
+    res.send(stringifyFile);
 });
 
 var server = app.listen(3000);
-
-app.post('/', function (req, res) {
-    console.log('Otrzymałem żądanie POST do strony głównej');
-    res.send('Hello POST!');
-});
-
-app.delete('/del_user', function (req, res) {
-    console.log('Otrzymałem żądanie DELETE do strony /del_user');
-    res.send('Hello DELETE!');
-});
-
-app.get('/list_user', function (req, res) {
-    console.log('Otrzymałem żądanie GET do strony /list_user');
-    res.send('Strona z listą użytkowników!');
-});
-
-app.get('/ab*cd', function(req, res) {
-    console.log('Otrzymałem żądanie GET do strony /ab*cd');
-    res.send('Wzór pasuje');
-});
-
-app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
-});
-*/
